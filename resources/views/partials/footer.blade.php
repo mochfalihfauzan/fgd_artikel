496d168c881c789de640d02fe9ba4fce643efa20
unknown <falihfauzan64@gmail.com>
2024-10-19 18:55:47 +0700

    create CRUD categories

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="bg-dark">
+    <div class="container py-5">
+        <div class="row">
+            <div class="col-md-12">
+                <p class="text-center text-white">© 2024 - Web Artikel FGD FYP Media</p>
+            </div>
+        </div>
+    </div>
+</footer>
--- routes/web.php
+++ routes/web.php
@@ -67,6 +67,7 @@ Route::get('/dashboard/posts/checkSlug', [DashboardPostController::class, 'check
 
 Route::resource('dashboard/posts', DashboardPostController::class)->middleware('auth');
 
+Route::get('/dashboard/categories/checkSlug', [AdminCategoryController::class, 'checkSlug'])->middleware('auth');
 Route::resource('dashboard/categories', AdminCategoryController::class)->except('show')
     ->middleware('admin');
 
